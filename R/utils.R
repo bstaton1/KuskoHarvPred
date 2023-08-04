2a919f452ac3ab14778b6b7cfb061329a5d56332
Ben <statonbe@gmail.com>
2023-08-04 07:38:56 -0700

    Create the get_var_name() fn

--- R/utils.R
+++ R/utils.R
@@ -161,3 +161,31 @@ get_mape = function(response, period) {
   # extract only the specific MAPE of interest and convert to a proportion
   errors[errors$response == response,paste0("MAPE_", period)]
 }
+
+#' Obtain the 'Pretty Name' of a Variable
+#'
+#' For printing axis labels, etc.
+#'
+#' @param var The variable name. Returns `NA` if not a valid variable.
+
+get_var_name = function(var) {
+  switch(var,
+         "effort" = "Drift Trips/Day",
+         "total_cpt" = "Salmon Catch/Trip",
+         "chinook_comp" = "% Chinook Salmon (Harvest)",
+         "chum_comp" = "% Chum Salmon (Harvest)",
+         "sockeye_comp" = "% Sockeye Salmon (Harvest)",
+         "day" = "Date",
+         "hours_open" = "Hours Open",
+         "not_first_day" = "Fished Yesterday?",
+         "weekend" = "Weekend?",
+         "p_before_noon" = "% Before Noon",
+         "total_btf_cpue" = "Total BTF CPUE",
+         "chinook_btf_comp" = "% Chinook Composition (BTF)",
+         "chum_btf_comp" = "% Chum Composition (BTF)",
+         "sockeye_btf_comp" = "% Sockeye Composition (BTF)",
+         "mean_Nwind" = "Northerly Wind Speed",
+         "mean_Ewind" = "Easterly Wind Speed",
+         NA
+  )
+}
