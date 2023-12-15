9b82c24b100b8c102a3b2098a791f8e236eba9e9
Ben <statonbe@gmail.com>
2023-12-15 00:30:25 -0800

    Add the BTF smoothing code

--- data-raw/make-pred-objs.R
+++ data-raw/make-pred-objs.R
@@ -33,14 +33,93 @@ misc_bank = list(
   weekend = c(FALSE, TRUE)
 )
 
+# function to return smoothed btf summaries using quantile generalized additive models fitted to btf data
+smooth_btf = function(stat, days = seq(min(KuskoHarvPred:::fit_data$day), max(KuskoHarvPred:::fit_data$day)), plus_minus = 1, plot = interactive()) {
 
+  # load the BTF data set
+  data(btf_master, package = "KuskoHarvData")
 
+  # name the btf stat: this is the name for output
+  btf_stat = stringr::str_replace(stat, "_", "_btf_")
 
+  # get the years to include
+  yr_range = range(lubridate::year(btf_master$date))
+  years = yr_range[1]:yr_range[2]
 
+  btf_summary = lapply(years, function(y) {
+    dates = KuskoHarvUtils::from_days_past_may31(days, y)
+    out = sapply(dates, KuskoHarvData:::summarize_btf, stat = stat, plus_minus = plus_minus)
+    out = data.frame(var = btf_stat, day = days, year = y, value = out)
+    return(out)
+  }); btf_summary = do.call(rbind, btf_summary)
 
+  ### smooth using quantile generalized additive model ###
 
+  # quantiles to smooth at
+  qu_seq = c(0.1, 0.25, 0.5, 0.75, 0.9)
 
+  # set the transformation function: keeps predictions positive and <1 for species composition
+  if(stat == "total_cpue") {
+    f = log; f_inv = exp
+    ylim = c(0, max(btf_summary$value, max(KuskoHarvPred:::fit_data[,btf_stat]))) * 1.25
+  } else {
+    f = qlogis; f_inv = plogis
+    # qlogis(1) = Inf; don't allow but retain information that it was near 100%
+    btf_summary$value[btf_summary$value == 1] = 0.99
+    ylim = c(0, 1)
+  }
 
+  # qlogis(0) = log(0) = -Inf; don't allow but retain information that was near 0%
+  btf_summary$value[btf_summary$value == 0] = 0.01
+
+  # fit the qgam for all elements in qu_seq
+  fits = suppressMessages(qgam::mqgam(f(value) ~ s(day), qu = qu_seq, data = btf_summary))
+
+  # create a prediction data set
+  pred_data = data.frame(day = with(btf_summary, seq(min(day), max(day))))
+
+  # obtain the fitted curve for each qgam
+  qu_preds = lapply(qu_seq, function(qu) {
+    tmp = pred_data
+    tmp$variable = btf_stat
+    tmp$qu = qu
+    tmp$pred = f_inv(qgam::qdo(fits, qu = qu, fun = predict, newdata = pred_data))
+    tmp
+  })
+  qu_preds = do.call(rbind, qu_preds)
+
+  if (plot) {
+    m = reshape2::dcast(btf_summary, day ~ year, value.var = "value")
+    m = m[,-1]
+    matplot(x = days, y = m, type = "l", col = scales::alpha("grey30", 0.5), xlab = "day", ylab = btf_stat, ylim = ylim)
+    lines(pred ~ day, data = subset(qu_preds, qu == 0.5))
+    lines(pred ~ day, data = subset(qu_preds, qu == 0.25), lty = 2)
+    lines(pred ~ day, data = subset(qu_preds, qu == 0.75), lty = 2)
+    lines(pred ~ day, data = subset(qu_preds, qu == 0.9), lty = 3)
+    lines(pred ~ day, data = subset(qu_preds, qu == 0.1), lty = 3)
+
+    points(x = KuskoHarvPred:::fit_data$day, y = KuskoHarvPred:::fit_data[,btf_stat], pch = 16)
+
+  }
+
+  out = qu_preds[,c("day", "qu", "pred")]
+  colnames(out) = c("day", "cat", btf_stat)
+  out$cat = paste0("q", out$cat * 100)
+  return(out)
+}
+
+# get BTF smoothed summaries
+smooth_total_cpue = smooth_btf(stat = "total_cpue")
+smooth_chinook_comp = smooth_btf(stat = "chinook_comp")
+smooth_chum_comp = smooth_btf(stat = "chum_comp")
+smooth_sockeye_comp = smooth_btf(stat = "sockeye_comp")
+
+# combine into one data frame
+btf_out = merge(smooth_total_cpue, smooth_chinook_comp) |>
+  merge(smooth_chum_comp) |>
+  merge(smooth_sockeye_comp)
+
+# function to build a prediction data set given the variables in the model and a range of days
 build_pred_data = function(vars, days = min(fit_data$day):max(fit_data$day)) {
 
   # extract the names of all BTF variables
@@ -75,7 +154,7 @@ build_pred_data = function(vars, days = min(fit_data$day):max(fit_data$day)) {
   return(out)
 }
 
-# build the prediction data set for each response variable
+# loop through response variables, and build the prediction data set
 pred_data = lapply(fit_lists, function(fit_list) build_pred_data(KuskoHarvPred:::find_variables(fit_list)))
 
 # add model averaged predictions
